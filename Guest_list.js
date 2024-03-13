dfd02b08dd95a309c60d32474bd0e3de467ebe79
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:30:06 +0500

    first commit

---
+++ Guest_list.js
@@ -0,0 +1,19 @@
+"use strict";
+let Guest_list = ['Kashif', 'Waqas', 'Naveed',];
+// for(let i=0; i<Guest_list.length; i++){
+//     console.log('Dear Mr. ' + Guest_list[i] + '\n\nIts my pleasure to invite you in our party.\n\nThankyou!\n\n') ;
+// }
+let absent_Guest = 'Kashif';
+let new_Guest = 'Azher';
+Guest_list[0] = new_Guest;
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + '\n\nIts my pleasure to invite you in our party.\n\nThankyou!\n\n');
+}
+console.log(`${absent_Guest} is not coming to the party.\n\n`);
+console.log('Good News! We find a big table so we are inviting 3 more guests.\n\n');
+Guest_list.unshift('Shahmir');
+Guest_list.splice(2, 0, 'Saam');
+Guest_list.push('Kashif');
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + '\n\nIts my pleasure to invite you in our party.\n\nThankyou!\n\n');
+}
